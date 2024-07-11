d9752769bbfe23402e320ef53548660d5edd32bb
Cinthyadelao <118902802+Cinthyadelao@users.noreply.github.com>
2024-07-11 16:05:13 +0200

    structure and functions

--- TheOffice/DataSource/ListEmployeeData.cs
+++ TheOffice/DataSource/ListEmployeeData.cs
@@ -14,6 +14,12 @@ namespace TheOffice.DataSource
         new Employee { EmployeeId = 2, Name = "Jim Halpert", DepartmentId = 2, Position = "Sales Representative" },
         new Employee { EmployeeId = 3, Name = "Pam Beesly", DepartmentId = 3, Position = "Receptionist" },
         new Employee { EmployeeId = 4, Name = "Dwight Schrute", DepartmentId = 2, Position = "Assistant to the Regional Manager" },
+        new Employee { EmployeeId = 5, Name = "Stanley Hudson", DepartmentId = 2, Position = "Sales Representative" },
+        new Employee { EmployeeId = 6, Name = "Phyllis Vance", DepartmentId = 2, Position = "Sales Representative" },
+        new Employee { EmployeeId = 7, Name = "Angela Martin", DepartmentId = 4, Position = "Accountant" },
+        new Employee { EmployeeId = 8, Name = "Kevin Malone", DepartmentId = 4, Position = "Accountant" },
+        new Employee { EmployeeId = 9, Name = "Oscar Martinez", DepartmentId = 4, Position = "Accountant" },
+        new Employee { EmployeeId = 10, Name = "Ryan Howard", DepartmentId = 2, Position = "Temp" }
     };
     }
 }
--- TheOffice/Program.cs
+++ TheOffice/Program.cs
@@ -9,32 +9,172 @@ namespace TheOffice
         static List<Employee> allEmployees = ListEmployeeData.Employees;
         static List<Department> allDepartments = ListDepartmentData.Departments;
         static List<Episode> allEpisodes = ListEpisodeData.Episodes;
-
         static void Main(string[] args)
         {
-            try
+            Console.WriteLine("Voici les Eployees groupées pour Departement");
+            showEmployeesGroupedByDepartment();
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+
+            ShowAnimation();
+            Console.WriteLine("Voici tout la data :)");
+            showAllData();
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+
+            ShowAnimation();
+            Console.WriteLine("Vous voulez savoir qui est \"the start\"?? regardez les acteurs qu'ont joue a minimun ce numero d'episodes:)");
+            ShowCharactersByMinimumEpisodeCount();
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+
+            ShowAnimation();
+            Console.WriteLine("Votre Liste sera converti en format JSON");
+            convListToJson();
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+
+            ShowAnimation();
+            Console.WriteLine("Votre Liste sera converti en format XML");
+            convListToXml();
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+
+            ShowAnimation();
+            Console.WriteLine("Faire une recherche sur plusieurs sources de données");
+            searche();
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+        }
+
+        static void showEmployeesGroupedByDepartment()
+        {
+            var employeesGroupedByDepartment = from employee in allEmployees
+                                               group employee by employee.DepartmentId into departmentGroup
+                                               select new
+                                               {
+                                                   DepartmentId = departmentGroup.Key,
+                                                   Employees = departmentGroup
+                                               };
+
+            foreach (var group in employeesGroupedByDepartment)
             {
-                // Conversion en XML
-                XmlConverter.ConvertListToXml();
-                Console.WriteLine("La conversion en XML est terminée.");
+                var departmentName = allDepartments
+                    .FirstOrDefault(dept => dept.DepartmentId == group.DepartmentId)?.Name;
 
-                // Conversion en JSON
-                JsonConverter.ConvertListToJson();
-                Console.WriteLine("La conversion en JSON est terminée.");
-
-                Console.WriteLine("Entrez le terme de recherche:");
-                string searchTerm = Console.ReadLine();
-
-                // Recherche globale dans les fichiers XML et JSON
-                Searcher.SearchInAllSources(searchTerm);
-
-                Console.WriteLine("Appuyez sur n'importe quelle touche pour quitter.");
-                Console.ReadKey();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Erreur dans le programme principal: {ex.Message}");
+                Console.WriteLine($"Department: {departmentName}");
+                foreach (var employee in group.Employees)
+                {
+                    Console.WriteLine($" - Employee: {employee.Name}, Position: {employee.Position}");
+                }
             }
         }
+
+        static void showAllData()
+        {
+            var employeeDetails = from employee in allEmployees
+                                  join department in allDepartments on employee.DepartmentId equals department.DepartmentId
+                                  select new
+                                  {
+                                      employee.Name,
+                                      employee.Position,
+                                      DepartmentName = department.Name,
+                                      Episodes = from episode in allEpisodes
+                                                 where episode.EmployeeIds.Contains(employee.EmployeeId)
+                                                 select episode.Title
+                                  };
+
+            foreach (var detail in employeeDetails)
+            {
+                string episodes = string.Join(", ", detail.Episodes);
+                Console.WriteLine($"{detail.Name} travaille dans le {detail.DepartmentName} et il est présent sur l'épisode {episodes}");
+                Console.WriteLine(" ");
+            }
+        }
+
+        static void ShowCharactersByMinimumEpisodeCount()
+        {
+            Console.WriteLine("Entrez le nombre minimum d'épisodes:");
+            string input = Console.ReadLine();
+            int minEpisodeCount = 0;
+
+            try
+            {
+                minEpisodeCount = int.Parse(input);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Entrée invalide.");
+                return;
+            }
+
+            var characters = allEmployees
+                .Select(employee => new
+                {
+                    employee.Name,
+                    employee.Position,
+                    EpisodeCount = allEpisodes.Count(e => e.EmployeeIds.Contains(employee.EmployeeId))
+                })
+                .Where(employee => employee.EpisodeCount >= minEpisodeCount);
+
+            Console.WriteLine($"Personnages apparaissant dans au moins {minEpisodeCount} épisodes:");
+
+            foreach (var character in characters)
+            {
+                Console.WriteLine($"{character.Name}, Poste: {character.Position}, Nombre d'épisodes: {character.EpisodeCount}");
+            }
+        }
+        static void ShowAnimation()
+        {
+            string text = "The Office ";
+            int width = Console.WindowWidth;
+
+            for (int i = 0; i < width + text.Length; i++)
+            {
+                Console.Clear();
+                if (i < width)
+                {
+                    Console.SetCursorPosition(i, 0);
+                }
+                else
+                {
+                    Console.SetCursorPosition(width - 1, 0);
+                }
+                Console.Write(text);
+                Thread.Sleep(10);
+            }
+            Console.Clear();
+        }
+
+
+
+        static void convListToXml()
+        {
+            // Conversion en XML
+            XmlConverter.ConvertListToXml();
+            Console.WriteLine("La conversion en XML est terminée.");
+        }
+
+        static void convListToJson()
+        {
+
+            // Conversion en JSON
+            JsonConverter.ConvertListToJson();
+            Console.WriteLine("La conversion en JSON est terminée.");
+
+        }
+
+        static void searche()
+        {
+            Console.WriteLine("Entrez le terme de recherche:");
+            string searchTerm = Console.ReadLine();
+
+            // Recherche globale dans les fichiers XML et JSON
+            Searcher.SearchInAllSources(searchTerm);
+
+            Console.WriteLine("Appuyez sur n'importe quelle touche pour quitter.");
+            Console.ReadKey();
+        }
+
     }
 }
